f033b3a04b96754769d4c3253e52962dd80ae1a0
--Sntyart <luistorresco@gmail.com>
2023-08-10 07:46:26 -0500

    FirstPythonProject

---
+++ inicio.py
@@ -0,0 +1,10 @@
+print("----- Calcular Definitiva -----")
+
+nombre = input("Nombre estudiante: ")
+
+n1=float(input("Nota 1: "))
+n2=float(input("Nota 2: "))
+n3=float(input("Nota 3: "))
+
+defnt = (n1+n2+n3)/3
+print(nombre+" Su nota definitiva es: "+ str(defnt))
